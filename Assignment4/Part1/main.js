df2a8084ec35846ea9fd68ef898df48ccdece77d
meetsGit <meetmahendrabhai.pandya@dcmail.ca>
2023-11-25 23:18:40 -0500

    Final commit I just changed an error.

--- Assignment4/Part1/main.js
+++ Assignment4/Part1/main.js
@@ -7,7 +7,7 @@
 const storyText = 'This all started in Japan when it was very cold in the whole village, and the temperature was 94 fahrenheit there was a :inserta: guy named Albert Einstein who wanted to prove the :insertb: wrong. Evantually he was able to prove them wrong. He even got :insertc: award for his results which wheighted 300 pounds ';
 const inserta = ['genius', 'bookworm', 'knowledgeable'];
 const insertb = ['Wave Theory', 'Particle nature of light', 'Law of gravity', 'Space Principals', 'Big Scientific statements'];
-const insertc = ['Nobel Price', 'Best Physicist of the history', 'solved a mystery']
+const insertc = ['Nobel Price', 'Best Physicist of the history',]
 
 const button = document.querySelector('.randomize');
 const paragraph = document.querySelector('.story');
