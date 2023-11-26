08cd62dfd97302eeac452fb1226e54732c2808f0
meetsGit <meetmahendrabhai.pandya@dcmail.ca>
2023-11-25 23:17:47 -0500

    Last Final Commit This is my last commit in which my whole part1 code is working properly.

--- Assignment4/Part1/main.js
+++ Assignment4/Part1/main.js
@@ -4,7 +4,7 @@
 // Date: 4th October 2023
 // Description: This is my main.js which will change the look of the webpage
 
-const storyText = 'This all started in Japan when there was chaos in the whole village, and the temperature was very cold like  there was a :inserta: guy named Albert Einstein who wanted to prove the :insertb: wrong. Evantually he was able to prove them wrong. He even got :insertc: award for his results ';
+const storyText = 'This all started in Japan when it was very cold in the whole village, and the temperature was 94 fahrenheit there was a :inserta: guy named Albert Einstein who wanted to prove the :insertb: wrong. Evantually he was able to prove them wrong. He even got :insertc: award for his results which wheighted 300 pounds ';
 const inserta = ['genius', 'bookworm', 'knowledgeable'];
 const insertb = ['Wave Theory', 'Particle nature of light', 'Law of gravity', 'Space Principals', 'Big Scientific statements'];
 const insertc = ['Nobel Price', 'Best Physicist of the history', 'solved a mystery']
@@ -33,6 +33,15 @@ function main()
         string = string.replaceAll('Albert Einstein', name);
       
     }
+
+    if (document.getElementById("uk").checked) 
+    {
+      const weight = `${Math.round(300*0.0714286)} stone`;
+      const temperature =  `${Math.round((94-32) * 5 / 9)} centigrade`;
+      string = string.replaceAll('94 fahrenheit', temperature);
+      string = string.replaceAll('300 pounds', weight);
+    }
+
     const x_random = random_item(inserta);
     const y_random = random_item(insertb);
     const z_random = random_item(insertc);
